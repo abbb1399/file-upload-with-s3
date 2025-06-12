638a3dd8db954fd9b11f580aab68e1f0bfdd3105
paul <abbb123@naver.com>
2025-06-12 23:08:17 +0900

    feat: 파일업로드 기본

---
+++ src/components/Uploader.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { FileRejection, useDropzone } from "react-dropzone";
+import { Card, CardContent } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+
+export function Uploader() {
+  const [files, setFiles] = useState<
+    Array<{
+      id: string;
+      file: File;
+      uploading: boolean;
+      progress: number;
+      key?: string;
+      isDeleting: boolean;
+      error: boolean;
+      objectUrl?: string;
+    }>
+  >([]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const onDropRejected = useCallback((fileRejections: FileRejection[]) => {
+    if (fileRejections.length > 0) {
+      const tooManyFiles = fileRejections.find(
+        (fileRejections) => fileRejections.errors[0].code === "too-many-files"
+      );
+
+      const fileTooLarge = fileRejections.find(
+        (fileRejections) => fileRejections.errors[0].code === "file-too-large"
+      );
+
+      if (tooManyFiles) {
+        toast.error("한번에 5개만 업로드 할 수 있습니다.");
+      }
+
+      if (fileTooLarge) {
+        toast.error("파일 크기가 너무 큽니다.");
+      }
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    onDropRejected,
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 5,
+    accept: {
+      "image/*": [],
+    },
+  });
+
+  return (
+    <Card
+      className={cn(
+        "relative border-2 border-dashed transition-colors duration-200 ease-in-out w-full h-64",
+        isDragActive
+          ? "border-primary bg-primary/10 border-solid"
+          : "border-border hover:border-primary"
+      )}
+      {...getRootProps()}
+    >
+      <CardContent className="flex flex-col items-center justify-center h-full w-full">
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>파일을 여기에 드래그 앤 드롭하거나 클릭하여 파일을 선택하세요.</p>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full w-full gap-y-3">
+            <p>
+              파일을 여기에 드래그 앤 드롭하거나 클릭하여 파일을 선택하세요.
+            </p>
+            <Button>파일 선택</Button>
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
